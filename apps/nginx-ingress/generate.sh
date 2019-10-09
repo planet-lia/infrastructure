785c73ae47dd26422e2041e7861794a50f4f6088
greenstatic <gregor@krmelj.xyz>
2019-10-09 11:44:19 +0200

    install nginx-ingress using argocd and helm template not kubespray

---
+++ apps/nginx-ingress/generate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Generates the nginx-ingress.yaml file from Helm chart. To override the values
+# simply edit the `helm_values.yaml` file. In order to change versions just change
+# the version in `VERSION.txt`
+
+set -e
+set -x
+
+# Move to script's dir
+cd $(dirname "$0")
+
+# helm repo update
+helm fetch stable/nginx-ingress --untar --version $(cat VERSION.txt)
+
+cd nginx-ingress
+
+helm template --name nginx-ingress --namespace nginx-ingress -f ../helm_values.yaml . > ../nginx-ingress.yaml
+
+cd ../
+rm -rf nginx-ingress
+
+set +x
+echo "Successfully generated nginx-ingress.yaml"
